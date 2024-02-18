dbd1b1816d71647113e78f6a43dcd7eb7617db18
Ranjan <ranjan22mohan@gmail.com>
2024-02-18 19:32:27 +0530

    new changes

--- UpdatedDropdown.java
+++ UpdatedDropdown.java
@@ -1,9 +1,19 @@
 package com.dropdown;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class UpdatedDropdown {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		 driver.get("https://spicejet.com");
+		  driver.manage().window().maximize();
+		  
+		  driver.findElement(By.xpath("//div[@class='css-1dbjc4n']//div//div[@class='css-1dbjc4n r-14lw9ot r-11u4nky r-z2wwpe r-1phboty r-rs99b7 r-1loqt21 r-13awgt0 r-ymttw5 r-5njf8e r-1otgn73']//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']")).click();
+		  
+		  
 
 	}
 
